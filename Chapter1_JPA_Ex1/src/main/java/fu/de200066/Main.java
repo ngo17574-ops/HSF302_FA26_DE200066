3f07814193b68edc61105da0259f31ce5a4bd1e3
Bao Huy <ngo17574@gmail.com>
2026-09-11 14:54:17 +0700

    feat(TODO2): Register the entity in persistence.xml

--- Chapter1_JPA_Ex1/src/main/java/fu/de200066/Main.java
+++ Chapter1_JPA_Ex1/src/main/java/fu/de200066/Main.java
@@ -1,5 +1,8 @@
 package fu.de200066;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
