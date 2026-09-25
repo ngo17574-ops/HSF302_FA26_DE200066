6d83cfd107c69bb22ea23b5a77cd6b5c9db354f6
Bao Huy <ngo17574@gmail.com>
2026-09-25 14:33:24 +0700

    feat(repository): add repositories and service layer skeleton
    Refs: TODO 4

---
+++ Slot6/ch4/src/main/java/com/hsf302/ch4/service/StudentServiceImpl.java
@@ -0,0 +1,16 @@
+package com.hsf302.ch4.service;
+
+import com.hsf302.ch4.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StudentServiceImpl implements StudentService {
+
+    private final StudentRepository studentRepository;
+
+    // Sẽ cài đặt method dần từ TODO 6
+}
